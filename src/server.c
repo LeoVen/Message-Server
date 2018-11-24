e8689a84c09a75d73e5727bb915fd6d1e7d19e60
LeoVen <lvenk26@gmail.com>
2018-11-24 00:43:03 -0200

    All server functions complete

--- src/server.c
+++ src/server.c
@@ -4,16 +4,52 @@
 int main(int argc, char *argv[])
 {
 	char *date = sf_date();
-
 	printf("%s\n", date);
-
 	free(date);
 
 	char *time = sf_time();
-
 	printf("%s\n", time);
-
 	free(time);
+
+	char *nodename = sf_nodename();
+	printf("%s\n", nodename);
+	free(nodename);
+
+	char *sysname = sf_sysname();
+	printf("%s\n", sysname);
+	free(sysname);
+
+	char *release = sf_release();
+	printf("%s\n", release);
+	free(release);
+
+	char *version = sf_version();
+	printf("%s\n", version);
+	free(version);
+
+	char *machine = sf_machine();
+	printf("%s\n", machine);
+	free(machine);
+
+	char *process = sf_process();
+	printf("%s\n", process);
+	free(process);
+
+	char *freeram = sf_freeram();
+	printf("%s\n", freeram);
+	free(freeram);
+
+	char *totalram = sf_totalram();
+	printf("%s\n", totalram);
+	free(totalram);
+
+	char *uptime = sf_uptime();
+	printf("%s\n", uptime);
+	free(uptime);
+
+	char *processors = sf_processors();
+	printf("%s\n", processors);
+	free(processors);
 
 	printf("Exiting...\n");
 	return 0;
--- src/sfunctions.c
+++ src/sfunctions.c
@@ -3,6 +3,8 @@
 #include "sfunctions.h"
 #include <time.h>
 
+#define BUFF_SIZE 100
+
 char *sf_date()
 {
 	time_t now;
@@ -11,9 +13,9 @@ char *sf_date()
 
 	struct tm *ct = localtime(&now);
 
-	char *str = malloc(sizeof(char) * 40);
+	char *str = malloc(sizeof(char) * BUFF_SIZE);
 
-	strftime(str, 20, "%d/%m/%Y", ct);
+	strftime(str, BUFF_SIZE, "%d/%m/%Y", ct);
 
 	return str;
 }
@@ -26,69 +28,146 @@ char *sf_time()
 
 	struct tm *ct = localtime(&now);
 
-	char *str = malloc(sizeof(char) * 40);
+	char *str = malloc(sizeof(char) * BUFF_SIZE);
 
-	strftime(str, 20, "%X", ct);
+	strftime(str, BUFF_SIZE, "%X", ct);
 
 	return str;
 }
 
 char *sf_nodename()
 {
+	struct utsname uts;
 
-	return NULL;
+	uname(&uts);
+
+	char *str = malloc(sizeof(char) * BUFF_SIZE);
+
+	sprintf(str, "%s", uts.nodename);
+
+	return str;
 }
 
 char *sf_sysname()
 {
+	struct utsname uts;
 
-	return NULL;
+	uname(&uts);
+
+	char *str = malloc(sizeof(char) * BUFF_SIZE);
+
+	sprintf(str, "%s", uts.sysname);
+
+	return str;
 }
 
 char *sf_release()
 {
+	struct utsname uts;
 
-	return NULL;
+	uname(&uts);
+
+	char *str = malloc(sizeof(char) * BUFF_SIZE);
+
+	sprintf(str, "%s", uts.release);
+
+	return str;
 }
 
 char *sf_version()
 {
+	struct utsname uts;
 
-	return NULL;
+	uname(&uts);
+
+	char *str = malloc(sizeof(char) * BUFF_SIZE);
+
+	sprintf(str, "%s", uts.version);
+
+	return str;
 }
 
 char *sf_machine()
 {
+	struct utsname uts;
 
-	return NULL;
+	uname(&uts);
+
+	char *str = malloc(sizeof(char) * BUFF_SIZE);
+
+	sprintf(str, "%s", uts.machine);
+
+	return str;
 }
 
 char *sf_process()
 {
+	struct sysinfo info;
 
-	return NULL;
+	sysinfo(&info);
+
+	char *str = malloc(sizeof(char) * BUFF_SIZE);
+
+	sprintf(str, "%d", info.procs);
+
+	return str;
 }
 
 char *sf_freeram()
 {
+	struct sysinfo info;
 
-	return NULL;
+	sysinfo(&info);
+
+	char *str = malloc(sizeof(char) * BUFF_SIZE);
+
+	sprintf(str, "%ld Kb", info.freeram * info.mem_unit / 1024);
+
+	return str;
 }
 
 char *sf_totalram()
 {
+	struct sysinfo info;
 
-	return NULL;
+	sysinfo(&info);
+
+	char *str = malloc(sizeof(char) * BUFF_SIZE);
+
+	sprintf(str, "%ld Kb", info.totalram * info.mem_unit / 1024);
+
+	return str;
 }
 
 char *sf_uptime()
 {
+	struct sysinfo info;
 
-	return NULL;
+	sysinfo(&info);
+
+	char *str = malloc(sizeof(char) * BUFF_SIZE);
+
+	int days, hours, mins;
+	long int  seconds = info.uptime % 60;
+
+	days = info.uptime / 86400;
+    hours = (info.uptime / 3600) - (days * 24);
+	mins = (info.uptime / 60) - (days * 1440) - (hours * 60);
+
+	char *h = (hours < 10) ? "0" : "";
+	char *m = (mins < 10) ? "0" : "";
+	char *s = (seconds < 10) ? "0" : "";
+
+	sprintf(str, "%d - %s%d:%s%d:%s%ld", days, h, hours, m, mins, s, seconds);
+
+	return str;
 }
 
 char *sf_processors()
 {
+	char *str = malloc(sizeof(char) * BUFF_SIZE);
 
-	return NULL;
+	sprintf(str, "%d", get_nprocs());
+
+	return str;
 }
